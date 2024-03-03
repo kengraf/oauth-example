89da8d3530bc7a0cbed16dbcbe6351a4fdb00e05
Ken Graf <kengraf57@gmail.com>
2024-03-03 09:02:41 -0500

    Create firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA0h8PIvh41O8OOoujMuTSHG-fSMZl_4-c",
+  authDomain: "kengraf-auth2.firebaseapp.com",
+  databaseURL: "https://kengraf-auth2.firebaseio.com",
+  storageBucket: "kengraf-auth2.appspot.com",
+};
+firebase.initializeApp(config);
